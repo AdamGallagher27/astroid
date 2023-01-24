00338efd56c766558e736304096e1e9feb84c481
Adam <N00211418@iadt.ie>
2023-01-24 13:37:08 +0000

    fetching nasa data from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
 
 // import styles from '@/styles/Home.module.css'
+import { useState, useEffect } from "react";
+
+
+const API_KEY: string = 'dsHDwoVaKhrkd78tefIhRQ1SedznGPz298f5Ml3P' 
+
 
 
 export default function Home() {
+  const [astroid, setAstroid] = useState<any>({})
+
+  const getAstroid = async () => {
+    try {
+      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${API_KEY}`)
+      const data = await response.json()
+      setAstroid(data.near_earth_objects['2015-09-07'])
+      console.log(data.near_earth_objects['2015-09-07'])
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getAstroid()
+    console.log(astroid)
+  }, [])
+  
+
+
   return (
     <>
+      <p>
+        {API_KEY}
+        {JSON.stringify(astroid)}
+      </p>
     </>
   )
 }
